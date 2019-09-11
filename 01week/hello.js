e130f016d5857931830bbe718d35da39f40f0467
TripKings <antonio.cicchese@gmail.com>
2019-09-10 20:34:14 -0500

    made changes to the colors

--- 01week/hello.js
+++ 01week/hello.js
@@ -1,7 +1,7 @@
 console.log("Hello Antonio");
 
-var color = 'red'
-var otherColor = 'blue'
+var color = 'blue'
+var otherColor = 'red'
 
 console.log("my color is" + color)
 console.log("my ohter color is", otherColor )
