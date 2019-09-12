6ab9fbd400b54aadebd1bd239bd6e9cd7a363d60
TripKings <antonio.cicchese@gmail.com>
2019-09-12 18:31:26 -0500

    working on my datatypes for class week 1 day 1

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,1 +1,36 @@
-new
+var now = new Date();
+
+console.log("The time at the beep is", now); // displays current day and time
+
+var n = 7;
+console.log ("the number is: " + n); // convert a number to a string
+
+const data = Number('7');
+console.log(data); // converts a string to a number
+
+var color = "Blue";
+
+color === "Blue";
+console.log(color); // datatype boolean
+
+var empty = null;
+console.log(empty) // datatype null
+
+var things;
+console.log(things) //datatype undefined
+
+var numbers = 22;
+console.log(numbers) //datatype numbers
+
+var nope = 1 / "One";
+console.log(nope); //datatype NaN
+
+var greeting = "Well hello there Antonio!"
+console.log(greeting); //datatype string
+
+var sum = 5 + 3;
+console.log(sum) // adds two numbers together
+
+
+
+
