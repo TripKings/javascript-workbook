187ea4c62a6cf5c5558bec56601bc138b3a8b3e6
TripKings <antonio.cicchese@gmail.com>
2019-09-24 19:27:08 -0500

    added our warm up for the day

---
+++ 03week/practice.js
@@ -0,0 +1,17 @@
+// for(var i=1; i<= 100; i++) {
+//     console.log(i)
+// }
+
+
+// for(var i=2; i<=100; i+=2) {
+//     console.log(i)
+// }
+
+var sum = 0;
+var num = 1;
+
+while(num=100){
+    sum = sum + num;
+    num = num +1;
+}
+console.log(num)
