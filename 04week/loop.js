5ad70b0030a7b22ad98ff6a02ae3994ee3f4600b
TripKings <antonio.cicchese@gmail.com>
2019-10-10 14:26:08 -0500

    finished loops

--- 04week/loop.js
+++ 04week/loop.js
@@ -22,3 +22,63 @@ function forInLoop() {
 }
 
 forInLoop();
+
+const arrayOfPersons = [
+    {
+        firstNmae: 'Jane',
+        lastName: 'Doe',
+        birthDate: 'Jan 5, 1925',
+        gender: 'female',
+        
+    },
+    {
+        firstNmae: 'John',
+        lastName: 'Smith',
+        birthDate: 'Feb 25, 1983',
+        gender: 'male',
+        
+    },
+    {
+        firstNmae: 'Pat',
+        lastName: 'Pitter',
+        birthDate: 'Sep 10, 1995',
+        gender: 'male',
+        
+    },
+    {
+        firstNmae: 'Shawna',
+        lastName: 'Kay',
+        birthDate: 'Jun 5, 2001',
+        gender: 'female',
+        
+    },
+
+]
+
+arrayOfPersons.map(function(items){
+    // console.log(items)
+})
+
+function checkGender() {
+    arrayOfPersons.filter(function(person) {
+        if(person.gender === 'male'){
+            // console.log(person)
+        }
+        
+    })
+
+}
+checkGender();
+
+function checkAge() {
+    arrayOfPersons.filter(function(person) {
+        var lastIndex = person.birthDate.length -1;
+        var date = person.birthDate.slice(lastIndex -4, lastIndex +1)
+        if(date <= 1990 ){
+            console.log(person)
+        }
+
+    })
+}
+
+checkAge()
