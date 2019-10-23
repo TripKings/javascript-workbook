5beac00284ff7712817dfbe3b02829cd3e4d0775
TripKings <antonio.cicchese@gmail.com>
2019-10-23 13:34:32 -0500

    working on dodgeball needing to add unit test

--- 06week/dodgeBall.js
+++ 06week/dodgeBall.js
@@ -55,14 +55,68 @@ const blueTeam = [];
 const redTeam = [];
 
 class Player {
-  constructor() {}
+  constructor(
+    id, 
+    name,
+    age,
+    skillSet, 
+    placeBorn,
+    canThrowBall,
+    canDodgeBall,
+    hasPaid,
+    isHealthy,
+    yearsExperience
+  ) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.skillSet = skillSet;
+    this.placeBorn = placeBorn;
+    this.canThrowBall = canThrowBall;
+    this.canDodgeBall = canDodgeBall;
+    this.hasPaid = hasPaid;
+    this.isHealthy = isHealthy, 
+    this.yearsExperience = yearsExperience;
+  }
+  joinRedTeam(redTeam) {
+    this.redTeam = redTeam;
+    player.redTeam.push(this);
+  }
+  joinBlueTeam(blueTeam) {
+    this.blueTeam = blueTeam;
+    player.blueTeam.push(this);
+  }
 }
-class blueTeammate {
-  constructor() {}
+class Teammate {
+  constructor(
+    id, 
+    name,
+    age, 
+    skillSet,
+    placeBorn,
+    canThrowBall,
+    canDodgeBall, 
+    hasPaid,
+    isHealthy,
+    yearsExperience,
+    mascot,
+    teamColor
+  ) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.skillSet = skillSet;
+    this.placeBorn = placeBorn;
+    this.canThrowBall = canThrowBall;
+    this.canDodgeBall = canDodgeBall;
+    this.hasPaid = hasPaid;
+    this.isHealthy = isHealthy, 
+    this.yearsExperience = yearsExperience, 
+    this.mascot = mascot;
+    this.teamColor = teamColor;
+  }
 }
-class redTeammate {
-  constructor() {}
-}
+
 
 const listPeopleChoices = () => {
   const listElement = document.getElementById("people");
@@ -72,6 +126,7 @@ const listPeopleChoices = () => {
     button.innerHTML = "Make Player";
     button.addEventListener("click", function() {
       makePlayer(person.id);
+      listElement.removeChild(li);
     });
     li.appendChild(button);
     li.appendChild(
@@ -81,39 +136,160 @@ const listPeopleChoices = () => {
   });
 };
 
+const makePlayer = id => {
+  const players = document.getElementById("players");
+  const findPlayer = arrOfPeople.find((entry) => {
+    return entry.id == id
+  });
+  console.log(findPlayer);
+  const playerIndex = arrOfPeople.indexOf(findPlayer);
+  console.log(
+    `this is the player index ${playerIndex}. Push this to the "Player list". Remove this fro the "list of Players`
+  );
+  const newPlayer = new Player(
+    findPlayer.id, 
+    findPlayer.name,
+    findPlayer.age,
+    findPlayer.skillSet,
+    findPlayer.placeBorn,
+    true,
+    true,
+    true, 
+    true, 
+    4
+  );
+  listOfPlayers.push(newPlayer);
+  arrOfPeople.splice(playerIndex, 1);
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newPlayer.name + " - "));
+
+  const blueButton = document.createElement("button");
+  blueButton.innerHTML = "Blue Team"
+  blueButton.addEventListener("click", function(){
+    makeBluePlayer(newPlayer.id);
+    players.removeChild(li);
+  });
+  li.appendChild(blueButton);
+
+  const redButton = document.createElement('button');
+  redButton.innerHTML = "Red Team"
+  redButton.addEventListener("click", function(){
+    makeRedPlayer(newPlayer.id);
+    players.removeChild(li);
+  });
+  li.appendChild(redButton);
+
+  players.append(li)
+};
+
+
+const makeBluePlayer = id => {
+  const playersBlue = document.getElementById("blue");
+  const selectBlue = listOfPlayers.find(function(pick) {
+    return pick.id == id;
+  });
+  const blueIndex = listOfPlayers.indexOf(selectBlue);
+
+  const newBluePlayer = new Teammate(
+    selectBlue.id,
+    selectBlue.name,
+    selectBlue.age,
+    selectBlue.skillSet,
+    selectBlue.placeBorn,
+    true,
+    true,
+    true,
+    true,
+    4,
+    "Dragon",
+    "Blue"
+  );
+  blueTeam.push(newBluePlayer);
+  listOfPlayers.splice(blueIndex, 1);
+
+  const li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(
+      newBluePlayer.name +
+        " - " +
+        "Mascot = " +
+        newBluePlayer.mascot +
+        " and Team Color is " +
+        newBluePlayer.teamColor
+    )
+  );
+  playersBlue.append(li);
+};
+
+
+
+const makeRedPlayer = id => {
+  const playerRed = document.getElementById("red");
+  const selectRed = listOfPlayers.find(pick => {
+    return pick.id == id;
+  });
+  const redIndex = listOfPlayers.indexOf(selectRed);
+
+  const newRedPlayer = new Teammate(
+    selectRed.id, 
+    selectRed.name, 
+    selectRed.age, 
+    selectRed.skillSet,
+    selectRed.placeBorn,
+    true, 
+    true, 
+    true, 
+    true,
+    4, 
+    "Sea Monkeys",
+    "Red"
+  );
+  redTeam.push(newRedPlayer);
+  listOfPlayers.splice(redIndex, 1);
+  const li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(
+      newRedPlayer.name + " - " + "Mascot = " + newRedPlayer.mascot + " and Team Color is " + newRedPlayer.teamColor
+    )
+  );
+  playerRed.append(li);
+};
+
+
+
 //create a higher order function that "removes" player from "list of people"
 //and places them in "dodgeball players"
 /*****problemos - we have it to where it moves the people and creates the actual text
 issue is all players are moving instead of individual and red player button is not button
 instead redplayer is being texted */
-const makePlayer = (id) => {
-  const dodgeBallPlayer = document.getElementById("players");
-  arrOfPeople.map(thePlayer => {
-    const li = document.createElement("li");
-    const redButton = document.createElement("button");
-    redButton.innerHTML = "Red Team";
-    redButton.addEventListener("clicked", function() {
-      chosenOne(thePlayer.id);
-    });
-    li.appendChild(redButton);
-    li.appendChild(
-        document.createTextNode(thePlayer.name + " - " + thePlayer.skillSet))
-    dodgeBallPlayer.append(li);
+// const makePlayer = (id) => {
+//   const dodgeBallPlayer = document.getElementById("players");
+//   arrOfPeople.map(thePlayer => {
+//     const li = document.createElement("li");
+//     const redButton = document.createElement("button");
+//     redButton.innerHTML = "Red Team";
+//     redButton.addEventListener("clicked", function() {
+//       chosenOne(thePlayer.id);
+//     });
+//     li.appendChild(redButton);
+//     li.appendChild(
+//         document.createTextNode(thePlayer.name + " - " + thePlayer.skillSet))
+//     dodgeBallPlayer.append(li);
 
-    const blueButton = document.createElement("button");
-    blueButton.innerHTML = "Blue Team";
-    blueButton.addEventListener('clicked', function() {
-      chosenOne(thePlayer.id);
-    });
-    li.appendChild(blueButton);
-    li.appendChild(
-      document.createTextNode(thePlayer.name + " - " + thePlayer.skillSet))
-    dodgeBallPlayer.append(li);
-  });
-  console.log(`li ${id} was clicked!`);
-};
+//     const blueButton = document.createElement("button");
+//     blueButton.innerHTML = "Blue Team";
+//     blueButton.addEventListener('clicked', function() {
+//       chosenOne(thePlayer.id);
+//     });
+//     li.appendChild(blueButton);
+//     li.appendChild(
+//       document.createTextNode(thePlayer.name + " - " + thePlayer.skillSet))
+//     dodgeBallPlayer.append(li);
+//   });
+//   console.log(`li ${id} was clicked!`);
+// };
 
-getElem
+
 
 //copy and paste below items for whiteboard and coding plan
 /* step one- need to make people move from "list of people"
